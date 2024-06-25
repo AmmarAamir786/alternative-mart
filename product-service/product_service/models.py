435f65f53a992476cdb78aa53725c3f007ba5d4b
Ammar Aamir <ammaraamir609@gmail.com>
2024-06-25 20:17:21 +0500

    first commit

--- product-service/product_service/models.py
+++ product-service/product_service/models.py
@@ -1,18 +1,15 @@
 from typing import Optional
 from sqlmodel import Field, SQLModel
 
+class ProductUpdate(SQLModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[float] = None
+    category: Optional[str] = None
 
-class Product (SQLModel, table=True):
+class Product(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     description: str
     price: float
     category: str
-
-
-class ProductUpdate(SQLModel):
-    name: str
-    description: str
-    price: float
-    category: str
-    
